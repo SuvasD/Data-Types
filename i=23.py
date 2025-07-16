19abe4c9954a99c48921e507fa9afd36936eff09
SuvasD <suvasdandu@gmail.com>
2025-07-16 11:52:01 -0700

    Add files via upload

--- i=23.py
+++ i=23.py
@@ -12,3 +12,10 @@ c="hello"
 print("type of c is ",type(c))
 d=True
 print("type of d is ",type(d))
+fname="suvas"
+lname="dandu"
+print("full name is ",fname+lname)
+print(fname[::3])
+print(fname[0::1])
+print(fname[::-1])
+print("first name is ",fname.upper())
