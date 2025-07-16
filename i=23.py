ab157584ac22d7038fa5e0913c74f809108215ff
SuvasD <suvasdandu@gmail.com>
2025-07-16 11:26:31 -0700

    Add files via upload

---
+++ i=23.py
@@ -0,0 +1,14 @@
+i=23
+f=23.6
+itf=float(i)
+print(itf)
+fti=int(f)
+print(fti)
+a=23
+print("type of a is ",type(a))
+b=45.7
+print("type of b is ",type(b))
+c="hello"
+print("type of c is ",type(c))
+d=True
+print("type of d is ",type(d))
